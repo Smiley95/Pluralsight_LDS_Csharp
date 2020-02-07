789113b22edc1515323b1e9b20092388d75480f6
Eya Taieb <ETaieb@se.linedata.com>
2020-02-07 18:52:48 +0100

    adding few notes

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,6 +6,9 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            //Book? book = new Book("b") : this means that this book variable can be nullable 
+            //if I want to control the behavior "reference types are by default none nullable" 
+            //you can remove the  <NullableContextOptions> tag in the .csproj file but highly not recommended 
             var book = new Book("book");
             while(true)
             {
